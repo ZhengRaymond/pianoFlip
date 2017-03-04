99e0ff8e30a5444f8692b959b521b1bd5ee0471a
christinasz <christinaszhang@gmail.com>
2017-03-04 16:44:30 -0500

    added support for octaves

--- keyRead.cpp
+++ keyRead.cpp
@@ -7,37 +7,39 @@ using namespace std;
 double d = 0;
 
 
-void keyPrint(int key, double dur) {
+void keyPrint(int key, int oct, double dur) {
 	if (key) {
-		cout << "note\t" << d << "\t" << dur << "\t" << key << "\t126" << endl;
+		cout << "note\t" << d << "\t" << dur << "\t" << oct << "\t126" << endl;
 	}
 	else {
-		cout << "rest\t" << d << "\t" << dur << "\t" << key << "\t126" << endl;
+		cout << "rest\t" << d << "\t" << dur << "\t" << -1 << "\t126" << endl;
 	}
 }
 
 int main() {
 	string key;
 	double dur;
+	int oct;
 
 	map<string, int>keys;
-	keys["C"] = 60;
-	keys["Cs"] = 61;
-	keys["D"] = 62;
-	keys["Ds"] = 63;
-	keys["E"] = 64;
-	keys["F"] = 65;
-	keys["Fs"] = 66;
-	keys["G"] = 67;
-	keys["Gs"] = 68;
-	keys["A"] = 69;
-	keys["As"] = 70;
-	keys["B"] = 71;
+	keys["C"] = 12;
+	keys["Cs"] = 13;
+	keys["D"] = 14;
+	keys["Ds"] = 15;
+	keys["E"] = 16;
+	keys["F"] = 17;
+	keys["Fs"] = 18;
+	keys["G"] = 19;
+	keys["Gs"] = 20;
+	keys["A"] = 21;
+	keys["As"] = 22;
+	keys["B"] = 23;
 
 	
 	while (cin >> key) {
+		cin >> oct;
 		cin >> dur;
-		keyPrint(keys[key], dur);
+		keyPrint(keys[key], (keys[key] + (12 * oct)), dur);
 		d += dur;
 	}
 }
